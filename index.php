cc58b6103cfb1d04b9232d17dfce09402279554a
Stinkijs <30879705+Stinkijs@users.noreply.github.com>
2021-11-10 00:07:43 +0100

    Ajout du fichier

---
+++ index.php
@@ -0,0 +1,8 @@
+<select class="">
+<?php
+	$recupUser = $bdd->prepare('SELECT pseudo FROM users');
+    $recupUser->execute();
+    while ($data = $recupUser->fetch())
+        echo "<option value=\"".$data['pseudo']."\">".$data['pseudo']."</option>";
+?>
+</select>
